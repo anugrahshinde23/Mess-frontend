98a54642d9586da91f4af613d7037bd73e98e041
anugrahshinde23 <andyy5325@gmail.com>
2026-02-12 17:13:02 +0530

    added logout in admin dashboard

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import { logoutApi } from '../services/auth.services';
+import { toast } from 'react-toastify';
+import { useAuth } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 const AdminDashboard = () => {
+
+  const {logout} = useAuth()
+
+  const navigate = useNavigate()
+
+
+     const handleLogout = async() => {
+        try {
+            const res = await logoutApi()
+    
+            console.log(res);
+    
+            toast.success(res.message)
+    
+            logout()
+            
+            setTimeout(() => {
+                navigate('/')
+            }, 1500);
+        } catch (err) {
+            toast.error("Failed to logout")
+        }
+       }
   return (
-    <div>AdminDashboard</div>
+    <>
+    
+    <p>Admin dashboard</p>
+    <button onClick={handleLogout}>logout</button>
+    
+    </>
   )
 }
 
