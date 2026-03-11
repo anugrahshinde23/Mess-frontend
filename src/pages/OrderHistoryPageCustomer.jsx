58f027ac884464620456c7faca4cb4a8c28910bd
anugrahshinde23 <andyy5325@gmail.com>
2026-03-11 17:27:46 +0530

    changes in timing

--- src/pages/OrderHistoryPageCustomer.jsx
+++ src/pages/OrderHistoryPageCustomer.jsx
@@ -96,15 +96,15 @@ const OrderHistoryPageCustomer = () => {
  ))}
 </div></td>
                   <td className="p-2 text-center">
-  {new Date(o.orderDate).toLocaleString("en-IN", {
-    timeZone: "Asia/Kolkata",
+                  {new Date(o.orderDate).toLocaleString("en-IN", {
+    timeZone: "UTC", // This will show 5:22 PM
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
-  })}
+})}
 </td>
                   <td className=" p-2 text-center">{o.status}</td>
                   <td className=" p-2 text-center">{o.orderShippingType}</td>
